66c89cb0c39aeb10e3aaf7f9e04473aed8186f12
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-08-23 16:15:19 -0700

    wroking on getting server up and ready

--- ppstructure/app.py
+++ ppstructure/app.py
@@ -51,4 +51,4 @@ async def predict_table(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80)
+    uvicorn.run(app, host="0.0.0.0", port=800)
